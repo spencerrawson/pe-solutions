43f0a841cafead6bbcb9af9178f4dea16007e028
no no <spencer@rawsonemail.com>
2023-09-28 20:00:52 -0500

    Spencer Rawson

--- pe13/pe13.cpp
+++ pe13/pe13.cpp
@@ -1,3 +1,8 @@
+/**
+ * Author: Spencer Rawson
+ * Date: 9/28/23
+ * Purpose: Solution for problem 13 (find first 10 digits of a large sum)
+*/
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -16,6 +21,7 @@ int main()
     //create sums array and set to 0
     int sums[50];
     for(int k = 0; k < 50; k++) { sums[k] = 0; }
+    //Add the value for each digit in each string to sum
     for(int i = 0; i < 100; i++)
     {
         for(int j = 0; j < 50; j++)
@@ -23,7 +29,7 @@ int main()
             sums[j] = sums[j] + input.at(i).at(j) - '0';
         }
     }
-    std::cout << sums[0] << std::endl;
+    //Move any values over 10 to the next highest digit starting from 1's place
     for(int x = 49; x >=0; x--)
     {
         if(x != 0 && sums[x] >= 10)
@@ -32,7 +38,7 @@ int main()
             sums[x] = sums[x]%10;
         }
     }
-    std::cout << sums[0] << std::endl;
+    //Print first 10 digits of sum (sums[0] has 3 digits)
     std::cout << sums[0] << sums[1] << sums[2] << sums[3] << sums[4] << sums[5] << sums[6] << sums[7];
     return 0;
 }
